766296769a46543b96c5b3f16ec17ddaf641b455
Héctor <hfaildeestevez@danielcastelao.org>
2021-11-23 10:29:01 +0100

    404 y css entregar para tarea

--- html/wp-content/themes/newFancy/404.php
+++ html/wp-content/themes/newFancy/404.php
@@ -20,12 +20,54 @@
 get_header()
 ?>
 <!--Mi 404 personalizado-->
-<h1 style="color:red">C MAMO LA PAGINA</h1>
-    <img src="<?php echo get_stylesheet_directory_uri();?>/images/cmamut.jpg">
+<h1 style="color:red"class="relative" >C MAMO LA PAGINA</h1>
+    <img src="<?php echo get_stylesheet_directory_uri();?>/images/cmamut.jpg" class="fixed">
 
-<h4>No hay nada que hacer, no se encuentra lo que buscas, igual que no se encuentran los sentimientos de ella hacia ti</h4>
-    <img src="<?php echo get_stylesheet_directory_uri();?>/images/gatogriton.jpg" width="360" height="341">
+<h4 class="sticky">No hay nada que hacer, no se encuentra lo que buscas, igual que no se encuentran los sentimientos de ella hacia ti</h4>
+    <img src="<?php echo get_stylesheet_directory_uri();?>/images/gatogriton.jpg" width="360" height="341" class="static">
 
+
+
+<!--El relleno es para darle mas volumen a la pagina y probar el sticky-->
+<h4>Glory, glory, Man United<br>
+    Glory, glory, Man United<br>
+    Glory, glory, Man United<br>
+    And the reds go marching on, on, on<br>
+    Just like the Busby Babes in Days gone by<br>
+    We'll keep the Red Flags flying high<br>
+    You've got to see yourself from far and wide<br>
+    You've got to hear the masses sing with pride<br>
+    <br>
+    <br>
+    United! Man United!<br>
+    We're the boys in Red and we're on our way to Wem-ber-ly<br>
+    Wem-ber-ly! Wem-ber-ly!<br>
+    We're the famous Man United and we're going to Wem-ber-ly<br>
+    Wem-ber-ly! Wem-ber-ly!<br>
+    We're the famous Man United and we're going to Wem-ber-ly<br>
+    In Seventy-Seven it was Docherty<br>
+    Atkinson will make it Eighty-Three<br>
+    And everyone will know just who we are<br>
+    They'll be singing Que Sera Sera<br>
+    <br>
+    <br>
+    United! Man United!<br>
+    We're the boys in Red and we're on our way to Wem-ber-ly<br>
+    Wem-ber-ly! Wem-ber-ly!<br>
+    We're the famous Man United and we're going to Wem-ber-ly<br>
+    Wem-ber-ly! Wem-ber-ly!<br>
+    We're the famous Man United and we're going to Wem-ber-ly<br>
+    <br>
+    <br>
+    Glory Glory Man United<br>
+    Glory Glory Man United<br>
+    Glory Glory Man United<br>
+    As the Reds Go Marching On! On! On!</h4><br>
+<h5>(ya para por favor es mucho lo que te aburres, que no se encuentra lo que buscas ya haz algo con tu vida)</h5>
+
+<?php
+    get_footer();
+?>
 </body>
 
 </html>
