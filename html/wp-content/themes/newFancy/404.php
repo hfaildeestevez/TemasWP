759e85f476c5a44e520af91b53c7b1e4d0e71e57
Héctor <hfaildeestevez@danielcastelao.org>
2021-11-23 09:30:42 +0100

    cambios en el 404 y style

--- html/wp-content/themes/newFancy/404.php
+++ html/wp-content/themes/newFancy/404.php
@@ -1,3 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>NEW FUNCY</title>
+    <link rel="stylesheet" href=<?php echo get_stylesheet_uri();?>>
+</head>
+
+
+<body class="background_404">
 <?php
 /**
  * The template for displaying the 404 template in the Twenty Twenty theme.
@@ -9,13 +19,15 @@
 
 get_header()
 ?>
- <!--Mi 404 personalizado-->
+<!--Mi 404 personalizado-->
 <h1 style="color:red">C MAMO LA PAGINA</h1>
     <img src="<?php echo get_stylesheet_directory_uri();?>/images/cmamut.jpg">
 
 <h4>No hay nada que hacer, no se encuentra lo que buscas, igual que no se encuentran los sentimientos de ella hacia ti</h4>
     <img src="<?php echo get_stylesheet_directory_uri();?>/images/gatogriton.jpg" width="360" height="341">
 
+</body>
 
+</html>
 
 
