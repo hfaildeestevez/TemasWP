faf984437edb772efbf27727e9c62fa7dbf05b1a
Héctor <hfaildeestevez@danielcastelao.org>
2021-11-26 13:13:04 +0100

    Loop1

--- html/wp-content/themes/newFancy/index.php
+++ html/wp-content/themes/newFancy/index.php
@@ -6,13 +6,20 @@
     <link rel="stylesheet" href=<?php echo get_stylesheet_uri();?>>
 </head>
 <body>
-<?php get_header() ?>
+<?php define( 'WP_USE_THEMES', false ); get_header(); ?>
 <?php include("header.php");?>
 <div id="cuerpo">
     <?php
     echo "<h1 class='morado'>Hola Funcy</h1>"
     ?>
 </div>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+    <small><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></small>
+<?php endwhile; else : ?>
+    <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
+
 <?php get_footer("footer.php")?>
 </body>
 </html>
