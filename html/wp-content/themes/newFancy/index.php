3aa124f7b7d7e48942fcf9d5e29269d1eae064e4
Héctor <hfaildeestevez@danielcastelao.org>
2021-11-30 10:05:15 +0100

    soporte para miniaturas

--- html/wp-content/themes/newFancy/index.php
+++ html/wp-content/themes/newFancy/index.php
@@ -13,9 +13,16 @@
     echo "<h1 class='morado'>Hola Funcy</h1>"
     ?>
 </div>
+
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
     <small><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></small>
+    <div class="entry">
+        <?php // the_content();
+        the_excerpt(); ?>
+    </div>
+    <p class="postmetadata"><?php _e( 'Posted in' ); ?> <?php the_category( ', ' ); ?></p>
+    </div> <!-- closes the first div box -->
 <?php endwhile; else : ?>
     <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
 <?php endif; ?>
